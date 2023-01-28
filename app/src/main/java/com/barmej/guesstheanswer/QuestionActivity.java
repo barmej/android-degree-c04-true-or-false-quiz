b8ca3c309f59e61c57112eac7cf0a3c30a79a6d1
Omar Albelbaisy <omar.albelbaisy@gmail.com>
2023-01-28 05:57:05 +0300

    Update gradle plugin, libraries, build tool and target sdk

--- app/src/main/java/com/barmej/guesstheanswer/QuestionActivity.java
+++ app/src/main/java/com/barmej/guesstheanswer/QuestionActivity.java
@@ -1,7 +1,7 @@
 package com.barmej.guesstheanswer;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class QuestionActivity extends AppCompatActivity {
 
